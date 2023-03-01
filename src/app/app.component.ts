f3e28d1bd7861b0467798371a417d4e1ff82ece1
Tim Lewis <tim.lewis@entelect.co.za>
2023-03-01 14:17:11 +0200

    simple list with interval

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-training';
+  title = 'Angular Training';
+  thingsToDo = [
+    'Learn JavaScript',
+    'Learn Angular',
+    'Learn Angular Material'
+  ]
+  thingsCompleted = []
+
+  constructor() {
+    setInterval(() => {
+      this.thingsToDo.push('Make coffee')
+    }, 1000)
+  }
+
+  summary(): string {
+    return `${this.thingsToDo.length} to do / ${this.thingsCompleted.length} done`
+  }
 }
